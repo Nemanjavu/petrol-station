7bf266c8321d0e3eb600feadb47b19145136a9ba
nemi <nemi@nemi-PC>
2013-12-16 15:08:40 +0100

    hhh

--- ZadnjaVerzija/src/com/example/zadnjaverzija/MainActivity.java
+++ ZadnjaVerzija/src/com/example/zadnjaverzija/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.zadnjaverzija;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemClickListener {
 
 	ListView listView;
 
@@ -24,6 +32,205 @@ public class MainActivity extends Activity {
 		ItemAdapterPetrolStation adapter = new ItemAdapterPetrolStation(this,
 				R.layout.priaz_activity_main, ids);
 		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(this);
+
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int id, long arg3) {
+
+		if (id == 0) {
+			AlertDialog.Builder izbornikBenziskeCrpke = new AlertDialog.Builder(
+					this);
+			izbornikBenziskeCrpke.setTitle("Izbornik");
+			String[] name = new String[] { "Nazovite Ina Mitnica",
+					"Navigacija", "Izlaz iz izbornika" };
+			izbornikBenziskeCrpke.setItems(name, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					switch (which) {
+					case 0:
+						Intent callMitnica = new Intent(Intent.ACTION_CALL);
+						callMitnica.setData(Uri.parse("tel:0914971308"));
+						startActivity(callMitnica);
+						finish();
+
+						break;
+
+					}
+
+					// TODO Auto-generated method stub
+
+				}
+			});
+			izbornikBenziskeCrpke.show();
+
+		} else if (id == 1) {
+			AlertDialog.Builder izbornikBenziskeCrpke = new AlertDialog.Builder(
+					this);
+			izbornikBenziskeCrpke.setTitle("Izbornik");
+			String[] name = new String[] { "Nazovite Ina Priljevo",
+					"Navigacija", "Izlaz iz izbornika" };
+			izbornikBenziskeCrpke.setItems(name, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					switch (which) {
+					case 0:
+						Intent callPriljevo = new Intent(Intent.ACTION_CALL);
+						callPriljevo.setData(Uri.parse("tel:0914971309"));
+						startActivity(callPriljevo);
+						finish();
+
+						break;
+
+					}
+
+					// TODO Auto-generated method stub
+
+				}
+			});
+			izbornikBenziskeCrpke.show();
+
+		} else if (id == 2) {
+			AlertDialog.Builder izbornikBenziskeCrpke = new AlertDialog.Builder(
+					this);
+			izbornikBenziskeCrpke.setTitle("Izbornik");
+			String[] name = new String[] { "Nazovite INA Borovo", "Navigacija",
+					"Izlaz iz izbornika" };
+			izbornikBenziskeCrpke.setItems(name, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					switch (which) {
+					case 0:
+						Intent callBorovo = new Intent(Intent.ACTION_CALL);
+						callBorovo.setData(Uri.parse("tel:0914971293"));
+						startActivity(callBorovo);
+						finish();
+
+						break;
+
+					}
+
+					// TODO Auto-generated method stub
+
+				}
+			});
+			izbornikBenziskeCrpke.show();
+
+		} else if (id == 3) {
+			AlertDialog.Builder izbornikBenziskeCrpke = new AlertDialog.Builder(
+					this);
+			izbornikBenziskeCrpke.setTitle("Izbornik");
+			String[] name = new String[] { "Nazovite Ina Petrol", "Navigacija",
+					"Izlaz iz izbornika" };
+			izbornikBenziskeCrpke.setItems(name, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					switch (which) {
+					case 0:
+						Intent callPetrol = new Intent(Intent.ACTION_CALL);
+						callPetrol.setData(Uri.parse("tel:032432253"));
+						startActivity(callPetrol);
+						finish();
+
+						break;
+
+					}
+
+					// TODO Auto-generated method stub
+
+				}
+			});
+			izbornikBenziskeCrpke.show();
+
+		} else if (id == 4) {
+			AlertDialog.Builder izbornikBenziskeCrpke = new AlertDialog.Builder(
+					this);
+			izbornikBenziskeCrpke.setTitle("Izbornik");
+			String[] name = new String[] { "Nazovite Vuka Benz", "Navigacija",
+					"Izlaz iz izbornika" };
+			izbornikBenziskeCrpke.setItems(name, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					switch (which) {
+					case 0:
+						Intent callVukaBenz = new Intent(Intent.ACTION_CALL);
+						callVukaBenz.setData(Uri.parse("tel:032432864"));
+						startActivity(callVukaBenz);
+						finish();
+
+						break;
+
+					}
+
+					// TODO Auto-generated method stub
+
+				}
+			});
+			izbornikBenziskeCrpke.show();
+
+		} else if (id == 5) {
+			AlertDialog.Builder izbornikBenziskeCrpke = new AlertDialog.Builder(
+					this);
+			izbornikBenziskeCrpke.setTitle("Izbornik");
+			String[] name = new String[] { "Nazovite OMV", "Navigacija",
+					"Izlaz iz izbornika" };
+			izbornikBenziskeCrpke.setItems(name, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					switch (which) {
+					case 0:
+						Intent callOMV = new Intent(Intent.ACTION_CALL);
+						callOMV.setData(Uri.parse("tel:032432829"));
+						startActivity(callOMV);
+						finish();
+
+						break;
+
+					}
+
+					// TODO Auto-generated method stub
+
+				}
+			});
+			izbornikBenziskeCrpke.show();
+
+		} else if (id == 6) {
+			AlertDialog.Builder izbornikBenziskeCrpke = new AlertDialog.Builder(
+					this);
+			izbornikBenziskeCrpke.setTitle("Izbornik");
+			String[] name = new String[] { "Nazovite Lukoil", "Navigacija",
+					"Izlaz iz izbornika" };
+			izbornikBenziskeCrpke.setItems(name, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					switch (which) {
+					case 0:
+						Intent callLukoil = new Intent(Intent.ACTION_CALL);
+						callLukoil.setData(Uri.parse("tel:032414769"));
+						startActivity(callLukoil);
+						finish();
+
+						break;
+
+					}
+
+					// TODO Auto-generated method stub
+
+				}
+			});
+			izbornikBenziskeCrpke.show();
+
+		}
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- ZadnjaVerzija/src/com/example/zadnjaverzija/UnosPodataka.java
+++ ZadnjaVerzija/src/com/example/zadnjaverzija/UnosPodataka.java
@@ -9,20 +9,21 @@ public class UnosPodataka {
 	public static void LoadData() {
 
 		Items = new ArrayList<ItemPetrolStation>();
-		Items.add(new ItemPetrolStation(1, "ina_logo.png", "INA Mitnica",
-				"Adresa: Bana Josipa Jelaèiæa BB","Radno vrijeme: 00:24h", "Telefon: 098988745"));
-		Items.add(new ItemPetrolStation(2, "ina_logo.png", "INA Priljevo",
-				"Priljevo BB", "00:24", "098988745"));
-		Items.add(new ItemPetrolStation(3, "ina_logo.png", "INA Borovo",
-				"Blage Zadre BB", "00:24", "098988745"));
-		Items.add(new ItemPetrolStation(4, "ina_petrol_logo.png", "INA Petrol",
-				"Priljevo BB", "00:24", "098988745"));
-		Items.add(new ItemPetrolStation(5, "vuka_logo.png", "Vuka Benz",
-				"Kudeljarska 6", "00:24", "098988745"));
-		Items.add(new ItemPetrolStation(6, "omv_logo.png", "OMV",
-				"Kudeljarska 4B", "00:24", "098988745"));
-		Items.add(new ItemPetrolStation(7, "lukoil_logo.png", "Lukoil",
-				"Trg Drvena pijaca BB", "00:24", "098988745"));
+		Items.add(new ItemPetrolStation(1, "gas_station.png", "INA Mitnica",
+				"Adresa: Bana Josipa Jelaèiæa BB", "Radno vrijeme: 00:24h",
+				"Telefon: 091/497-1308"));
+		Items.add(new ItemPetrolStation(2, "gas_station.png", "INA Priljevo",
+				"Priljevo BB", "00:24", "091/497-1309"));
+		Items.add(new ItemPetrolStation(3, "gas_station.png", "INA Borovo",
+				"Blage Zadre BB", "00:24", "091/497-1293"));
+		Items.add(new ItemPetrolStation(4, "gas_station.png", "INA Petrol",
+				"Kudeljarska 3", "00:24", "032/432-253"));
+		Items.add(new ItemPetrolStation(5, "gas_station.png", "Vuka Benz",
+				"Kudeljarska 6", "00:24", "032/432-864"));
+		Items.add(new ItemPetrolStation(6, "gas_station.png", "OMV",
+				"Kudeljarska 4B", "00:24", "032/432-829"));
+		Items.add(new ItemPetrolStation(7, "gas_station.png", "Lukoil",
+				"Trg Drvena pijaca BB", "00:24", "032/414-769"));
 	}
 
 	public static ItemPetrolStation GetbyId(int id) {
