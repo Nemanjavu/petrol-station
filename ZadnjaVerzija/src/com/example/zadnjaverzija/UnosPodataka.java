2b0b43c2b389783b1bb59a6f68ad27828eaa215c
nemi <nemi@nemi-PC>
2013-12-16 19:25:27 +0100

    rrd

--- ZadnjaVerzija/src/com/example/zadnjaverzija/UnosPodataka.java
+++ ZadnjaVerzija/src/com/example/zadnjaverzija/UnosPodataka.java
@@ -13,17 +13,23 @@ public class UnosPodataka {
 				"Adresa: Bana Josipa Jelaèiæa BB", "Radno vrijeme: 00:24h",
 				"Telefon: 091/497-1308"));
 		Items.add(new ItemPetrolStation(2, "gas_station.png", "INA Priljevo",
-				"Priljevo BB", "00:24", "091/497-1309"));
+				"Adresa: Priljevo BB", "Radno vrijeme: 00:24",
+				"Telefon: 091/497-1309"));
 		Items.add(new ItemPetrolStation(3, "gas_station.png", "INA Borovo",
-				"Blage Zadre BB", "00:24", "091/497-1293"));
+				"Adresa: Blage Zadre BB", "Radno vrijeme: 00:24",
+				"Telefon: 091/497-1293"));
 		Items.add(new ItemPetrolStation(4, "gas_station.png", "INA Petrol",
-				"Kudeljarska 3", "00:24", "032/432-253"));
+				"Adresa: Kudeljarska 3", "Radno vrijeme: 00:24",
+				"Telefon: 032/432-253"));
 		Items.add(new ItemPetrolStation(5, "gas_station.png", "Vuka Benz",
-				"Kudeljarska 6", "00:24", "032/432-864"));
+				"Adresa: Kudeljarska 6", "Radno vrijeme: 00:24",
+				"Telefon: 032/432-864"));
 		Items.add(new ItemPetrolStation(6, "gas_station.png", "OMV",
-				"Kudeljarska 4B", "00:24", "032/432-829"));
+				"Adresa: Kudeljarska 4B", "Radno vrijeme: 00:24",
+				"Telefon: 032/432-829"));
 		Items.add(new ItemPetrolStation(7, "gas_station.png", "Lukoil",
-				"Trg Drvena pijaca BB", "00:24", "032/414-769"));
+				"Adresa: Trg Drvena pijaca BB", "Radno vrijeme: 00:24",
+				"Telefon: 032/414-769"));
 	}
 
 	public static ItemPetrolStation GetbyId(int id) {
